5e6d0add3c8b966b7dab43d695f190a9955145fe
arunaShakthi <arunashakthi002@gmail.com>
2025-01-03 04:39:14 +0530

    remove function

--- controllers/productController.js
+++ controllers/productController.js
@@ -59,18 +59,41 @@ const listProduct = async (req, res) => {
 
         console.log(error)
         res.json({success:false, message:error.message})
-        
+
     }
 
 }
 
 // function for remove product
 const removeProduct = async (req, res) => {
+
+    try {
+        
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true, message:"Product Removed"})
+
+    } catch (error) {
+
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
     
 }
 
 // function for single product info
 const singleProduct = async (req, res) => {
+
+    try {
+        
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
+        res.json({success:true, product})
+
+    } catch (error) {
+
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
     
 }
 
